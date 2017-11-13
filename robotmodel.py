ea1a3ad40caee00f74589ab5b30a60d86e84d84f
Xomak <mail@xomak.net>
2017-11-13 15:54:58 +0300

    Refactor robot model, add model without compass

--- robotmodel.py
+++ robotmodel.py
@@ -2,7 +2,6 @@ from collections import namedtuple
 from typing import Tuple
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 from data_source import PhoneDataSource, RobotDataSource, CameraDataSource
 from sensors_bag import SensorsBag
@@ -16,7 +15,7 @@ class RobotModel:
     RPS_TO_RADIANS = 2 * np.pi
     WHEEL_RADIUS = 2.75
     WHEEL_DISTANCE = 14.5 / 2
-    DISTANCE_TO_WALL = 13
+    SONAR_ENABLED_ANGLE = 45
 
     StepData = namedtuple('StepData', ('observations',
                                        'control',
@@ -25,11 +24,20 @@ class RobotModel:
                                        'transition_function',
                                        'observations_function'))
 
-    def __init__(self, robot_data: RobotDataSource, phone_data: PhoneDataSource, camera_data: CameraDataSource):
+    ProcessDispersions = namedtuple('ProcessDispersions', ('x', 'y', 'theta'))
+
+    ObservationDispersions = namedtuple('ObservationDispersionsWithoutCompass',
+                                        ('camera_x', 'camera_y', 'angle_gyro',
+                                         'sonar', 'sonar_inf'))
+
+    def __init__(self, robot_data: RobotDataSource, camera_data: CameraDataSource):
 
         self.camera_data = camera_data
-        self.phone_data = phone_data
         self.robot_data = robot_data
+        self.distance_to_wall = 13
+        self.initial_dispersions = self.ProcessDispersions(25, 25, 4)
+        self.process_dispersions = self.ProcessDispersions(225, 225, 225)
+        self.observation_dispersions = self.ObservationDispersions(100, 100, 4, 4, 100000)
 
     def get_transition_function_for_delta_time(self, delta_time: float):
 
@@ -47,36 +55,36 @@ class RobotModel:
         process_covariance = self.process_covariance
         transition_function = self.get_transition_function_for_delta_time(time_delta)
         observations_function = self.observations_function
-        observations_covariance = self.observations_covariance(state, observations)
+        observations_covariance = self.observations_covariance(observations)
         result = RobotModel.StepData(observations, control, observations_covariance,
                                      process_covariance, transition_function, observations_function)
         return result
 
     def add_to_bag(self, step_data: 'RobotModel.StepData', sensor_bag: SensorsBag):
+        # Without compass
+
         sensor_bag.add_parameter('x', 'camera', step_data.observations[0])
         sensor_bag.add_parameter('y', 'camera', step_data.observations[1])
-        sensor_bag.add_parameter('angle', 'compass', step_data.observations[2])
-        sensor_bag.add_parameter('angle', 'gyroscope', step_data.observations[3])
-        sensor_bag.add_parameter('sonar_distance', 'sonar', step_data.observations[4])
-        distance = step_data.observations[4]
-        angle = np.deg2rad(RobotModel.normalize_angle(step_data.observations[3]))
-        sensor_bag.add_parameter('y', 'sonar', RobotModel.get_y_from_measured(distance, angle))
+        sensor_bag.add_parameter('angle', 'gyroscope', step_data.observations[2])
+        sensor_bag.add_parameter('sonar_distance', 'sonar', step_data.observations[3])
+        distance = step_data.observations[3]
+        angle = np.deg2rad(self.normalize_angle(step_data.observations[2]))
+        sensor_bag.add_parameter('y', 'sonar', self.get_y_from_measured(distance, angle))
         return sensor_bag
 
     def get_observations_and_control_for(self, timestamp: float) -> Tuple[np.array, np.array]:
+        # Without compass
 
         camera_row = self.camera_data.find_nearest_to_timestamp(timestamp)
         camera_x = camera_row[1]
         camera_y = camera_row[2]
-        phone_row = self.phone_data.find_nearest_to_timestamp(timestamp)
-        phone_angle = RobotModel.normalize_angle(phone_row[1])
         robot_row = self.robot_data.find_nearest_to_timestamp(timestamp)
         robot_sonar = robot_row[1]
-        robot_angle = RobotModel.normalize_angle(robot_row[2])
+        robot_angle = self.normalize_angle(robot_row[2])
         left_motor = robot_row[3]
         right_motor = robot_row[4]
 
-        observations = np.array([camera_x, camera_y, phone_angle, robot_angle, robot_sonar])
+        observations = np.array([camera_x, camera_y, robot_angle, robot_sonar])
         control = np.array([left_motor, right_motor])
 
         return observations, control
@@ -98,34 +106,33 @@ class RobotModel:
         velocity = RobotModel.WHEEL_RADIUS / 2 * (left_motor_speed + right_motor_speed)
 
         # Angular velocity in radians/s
-        angular_velocity = RobotModel.WHEEL_RADIUS / (2 * RobotModel.WHEEL_DISTANCE) * (left_motor_speed - right_motor_speed)
-
-        #print(velocity, angular_velocity)
+        angular_velocity = RobotModel.WHEEL_RADIUS / (2 * RobotModel.WHEEL_DISTANCE) * (
+            left_motor_speed - right_motor_speed)
 
         new_angle_radians = theta_radians + time_delta * angular_velocity
 
         x += time_delta * velocity * np.cos(new_angle_radians)
         y += time_delta * velocity * np.sin(new_angle_radians)
 
-        # x += time_delta * velocity * np.cos(theta_radians)
-        # y -= time_delta * velocity * np.sin(theta_radians)
         new_angle = np.degrees(new_angle_radians)
 
-        new_angle = (new_angle + 360) % 360
+        new_angle = RobotModel.normalize_angle(new_angle)
 
         return np.array([x, y, new_angle])
 
-    @staticmethod
-    def get_y_from_measured(distance: float, angle_radians: float):
-        return np.cos(angle_radians) * distance - RobotModel.DISTANCE_TO_WALL
+    def get_y_from_measured(self, distance: float, angle_radians: float):
+        return np.cos(angle_radians) * distance - self.distance_to_wall
 
-    @staticmethod
-    def get_measured_from_y_and_angle(y: float, angle_radians: float):
-        return y / np.cos(angle_radians) + RobotModel.DISTANCE_TO_WALL
+    def get_measured_from_y_and_angle(self, y: float, angle_radians: float):
+        return y / np.cos(angle_radians) + self.distance_to_wall
 
     @property
     def initial_covariance(self):
-        return np.matrix("25 0 0; 0 25 0; 0 0 1")
+        matrix = np.asmatrix(np.zeros((3, 3)))
+        matrix[0, 0] = self.initial_dispersions.x
+        matrix[1, 1] = self.initial_dispersions.y
+        matrix[2, 2] = self.initial_dispersions.theta
+        return matrix
 
     @property
     def initial_state(self):
@@ -133,27 +140,107 @@ class RobotModel:
 
     @property
     def process_covariance(self):
-        return np.matrix("0.25 0 0; 0 0.25 0; 0 0 0.25")
+        matrix = np.asmatrix(np.zeros((3, 3)))
+        matrix[0, 0] = self.process_dispersions.x
+        matrix[1, 1] = self.process_dispersions.y
+        matrix[2, 2] = self.process_dispersions.theta
+        return matrix
 
-    def observations_covariance(self, state: np.array, observations: np.array):
-        sonar_degree = 75
+    @property
+    def observations_dimension(self):
+        return 4
 
-        covariance = np.matrix(np.zeros((5, 5)))
-        covariance[0, 0] = 16
-        covariance[1, 1] = 16
-        covariance[2, 2] = 360*360
-        covariance[3, 3] = 25
+    def observations_function(self, state: np.array):
+        # WITHOUT
+        # Observations: Camera X, Camera Y, Gyroscope angle, Sonar distance
+        observed = np.ndarray((4,))
+        observed[0] = state[0]  # camera x = state.x
+        observed[1] = state[1]  # camera y = state.y
+        observed[2] = state[2]  # Gyroscope angle = theta
+        observed[3] = self.get_measured_from_y_and_angle(state[1],
+                                                         np.deg2rad(state[2]))  # Sonar measured, calc from y and theta
+        return observed
 
-        if (state[2] < sonar_degree or state[2] > (360-sonar_degree)) and observations[4] < 100 and observations[4] > 13:
-            covariance[4, 4] = 2
+    def observations_covariance(self, observations: np.array):
+        # Without compass
+        covariance = np.matrix(np.zeros((4, 4)))
+        covariance[0, 0] = self.observation_dispersions.camera_x
+        covariance[1, 1] = self.observation_dispersions.camera_y
+        covariance[2, 2] = self.observation_dispersions.angle_gyro
+
+        sonar_distance = observations[3]
+        angle = observations[2]
+
+        if (angle < self.SONAR_ENABLED_ANGLE or angle > (360 - self.SONAR_ENABLED_ANGLE)) \
+                and 100 > sonar_distance > self.distance_to_wall:
+            covariance[3, 3] = self.observation_dispersions.sonar
         else:
-            covariance[4, 4] = 10000000
+            covariance[3, 3] = self.observation_dispersions.sonar_inf
 
         return covariance
+
+
+class RobotModelWithCompass(RobotModel):
+    ObservationDispersionsWithCompass = namedtuple('ObservationDispersions',
+                                                   ('camera_x', 'camera_y', 'angle_gyro',
+                                                    'sonar', 'sonar_inf', 'angle_compass'))
+
+    def __init__(self, robot_data: RobotDataSource, camera_data: CameraDataSource, phone_data: PhoneDataSource):
+        super().__init__(robot_data, camera_data)
+        self.observation_dispersions = self.ObservationDispersionsWithCompass(100, 100, 4, 4, 100000, 360*360)
+        self.phone_data = phone_data
 
     @property
     def observations_dimension(self):
         return 5
+
+    def add_to_bag(self, step_data: 'RobotModel.StepData', sensor_bag: SensorsBag):
+        sensor_bag.add_parameter('x', 'camera', step_data.observations[0])
+        sensor_bag.add_parameter('y', 'camera', step_data.observations[1])
+        sensor_bag.add_parameter('angle', 'compass', step_data.observations[2])
+        sensor_bag.add_parameter('angle', 'gyroscope', step_data.observations[3])
+        sensor_bag.add_parameter('sonar_distance', 'sonar', step_data.observations[4])
+        distance = step_data.observations[4]
+        angle = np.deg2rad(RobotModel.normalize_angle(step_data.observations[3]))
+        sensor_bag.add_parameter('y', 'sonar', self.get_y_from_measured(distance, angle))
+        return sensor_bag
+
+    def get_observations_and_control_for(self, timestamp: float) -> Tuple[np.array, np.array]:
+
+        camera_row = self.camera_data.find_nearest_to_timestamp(timestamp)
+        camera_x = camera_row[1]
+        camera_y = camera_row[2]
+        phone_row = self.phone_data.find_nearest_to_timestamp(timestamp)
+        phone_angle = self.normalize_angle(phone_row[1])
+        robot_row = self.robot_data.find_nearest_to_timestamp(timestamp)
+        robot_sonar = robot_row[1]
+        robot_angle = self.normalize_angle(robot_row[2])
+        left_motor = robot_row[3]
+        right_motor = robot_row[4]
+
+        observations = np.array([camera_x, camera_y, phone_angle, robot_angle, robot_sonar])
+        control = np.array([left_motor, right_motor])
+
+        return observations, control
+
+    def observations_covariance(self, observations: np.array):
+
+        covariance = np.matrix(np.zeros((5, 5)))
+        covariance[0, 0] = self.observation_dispersions.camera_x
+        covariance[1, 1] = self.observation_dispersions.camera_y
+        covariance[2, 2] = self.observation_dispersions.angle_compass
+        covariance[3, 3] = self.observation_dispersions.angle_gyro
+
+        sonar_distance = observations[4]
+        angle = observations[3]
+
+        if (angle < self.SONAR_ENABLED_ANGLE or angle > (360 - self.SONAR_ENABLED_ANGLE)) \
+                and 100 > sonar_distance > self.distance_to_wall:
+            covariance[4, 4] = self.observation_dispersions.sonar
+        else:
+            covariance[4, 4] = self.observation_dispersions.sonar_inf
+
+        return covariance
 
     def observations_function(self, state: np.array):
         # Observations: Camera X, Camera Y, Compass angle, Gyroscope angle, Sonar distance
@@ -162,5 +249,6 @@ class RobotModel:
         observed[1] = state[1]  # camera y = state.y
         observed[2] = state[2]  # Compass angle = theta
         observed[3] = state[2]  # Gyroscope angle = theta
-        observed[4] = RobotModel.get_measured_from_y_and_angle(state[1], np.deg2rad(state[2]))  # Sonar measured, calc from y and theta
+        observed[4] = self.get_measured_from_y_and_angle(state[1],
+                                                         np.deg2rad(state[2]))  # Sonar measured, calc from y and theta
         return observed
